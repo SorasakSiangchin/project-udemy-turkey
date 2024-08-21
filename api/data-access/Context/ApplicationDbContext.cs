d8605c41ccb5c2bf6626c11d1315aaecc58b3e9b
SUPALAI\sorasak.sia <sorasak.sia@supalai.com>
2024-08-21 17:07:44 +0700

    first commit

---
+++ api/data-access/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using data_access.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace data_access.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ api/data-access/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace data_access.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
